e2287b78dbe90d3864ce53974e71dc5718e739b0
gallayl <gallay.lajos@gmail.com>
2018-08-07 15:12:26 +0200

    added session action tests, updated dependencies

---
+++ packages/http-api/test/actions/LogoutActionTests.ts
@@ -0,0 +1,25 @@
+import { expect } from "chai";
+import { LogoutAction } from "../../src/Actions/Logout";
+import { IdentityService } from "../../src/IdentityService";
+
+export const logoutActionTests = describe("LogoutAction", () => {
+    it("should be constructed without parameters", () => {
+        const c = new LogoutAction(new IdentityService({} as any));
+        expect(c).to.be.instanceof(LogoutAction);
+    });
+
+    it("exec", async () => {
+        const c = new LogoutAction({
+            cookieLogout: async () => undefined,
+        } as any);
+        await c.exec({
+            method: "POST",
+            headers: {
+                cookie: "",
+            },
+        } as any, {
+            writeHead: () => undefined,
+            end: () => undefined,
+        } as any);
+    });
+});
