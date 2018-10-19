3caa6bfeb1f0f9f16e501b712d0cca8c90c10990
Gallay Lajos <gallay.lajos@LIVE.COM>
2018-10-19 11:06:00 +0200

    fix: IContext isAuthorized

--- packages/core/src/Models/IContext.ts
+++ packages/core/src/Models/IContext.ts
@@ -1,9 +1,10 @@
 import { Injector } from "@furystack/inject";
+import { IRole } from "./IRole";
 import { IUser } from "./IUser";
 
 export interface IContext {
     getCurrentUser(): Promise<IUser>;
     isAuthenticated(): Promise<boolean>;
-    isAuthorized(...claims: string[]): Promise<boolean>;
+    isAuthorized(...roles: IRole[]): Promise<boolean>;
     getInjector: () => Injector;
 }
