76608809a2f3c63309f8d5e7380e9b9cf7b43d5a
Gallay Lajos <gallay.lajos@LIVE.COM>
2018-11-10 11:22:11 +0100

    merge

--- packages/core/src/Models/IRole.ts
+++ packages/core/src/Models/IRole.ts
@@ -1,5 +1,5 @@
 export interface IRole {
-    Id: number;
     Name: string;
+    DisplayName?: string;
     Description?: string;
 }
--- packages/core/src/SystemRoles.ts
+++ packages/core/src/SystemRoles.ts
@@ -2,14 +2,14 @@ import { IRole } from "./Models/IRole";
 
 export class SystemRoles {
     public static Visitors: IRole = {
-        Id: 1,
-        Name: "The user is a Visitor",
+        Name: "Visitor",
+        DisplayName: "The user is a Visitor",
         Description: "Roles applied for non-authenticated users",
     };
 
     public static AuthenticatedUsers: IRole = {
-        Id: 2,
-        Name: "Authenticated users",
+        Name: "Authenticated",
+        DisplayName: "Authenticated users",
         Description: "The user is logged in with a valid account",
     };
 
