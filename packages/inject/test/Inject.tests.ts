380ae2f36db7dcad665ed6a6b76fa8333496af6e
gallayl <gallay.lajos@gmail.com>
2018-11-14 17:01:07 +0100

    refactor to jest

--- packages/inject/test/Inject.tests.ts
+++ packages/inject/test/Inject.tests.ts
@@ -1,12 +1,11 @@
-import { expect } from "chai";
 import { Injectable } from "../src";
 
 // tslint:disable:max-classes-per-file
-export const injectTests: Mocha.Suite = describe("Tests", () => {
+export const injectTests = describe("Tests", () => {
     it("Should decorate classes", () => {
         @Injectable() class Alma { }
         const a = new Alma();
-        expect(a).to.be.instanceof(Alma);
+        expect(a).toBeInstanceOf(Alma);
     });
 
     it("Should resolve ctor parameters", () => {
--- packages/inject/test/Injector.tests.ts
+++ packages/inject/test/Injector.tests.ts
@@ -1,5 +1,4 @@
 import { IDisposable, usingAsync } from "@sensenet/client-utils";
-import { expect } from "chai";
 import { Injectable } from "../src/Injectable";
 import { Injector } from "../src/Injector";
 
@@ -8,20 +7,20 @@ import { Injector } from "../src/Injector";
 export const injectorTests = describe("Injector", () => {
     it("Shold be constructed", () => {
         const i = new Injector();
-        expect(i).to.be.instanceof(Injector);
+        expect(i).toBeInstanceOf(Injector);
     });
 
     it("Parent should be the default instance, if not specified", () => {
         const i = new Injector();
         // tslint:disable-next-line:no-string-literal
-        expect(i["options"]["parent"]).to.be.eq(Injector.Default);
+        expect(i["options"]["parent"]).toBe(Injector.Default);
     });
 
     it("Should throw an error on circular dependencies", () => {
         const i = new Injector({});
         @Injectable()
         class InstanceClass { constructor(private _ohgodno: InstanceClass) { /** */ } }
-        expect(() => i.GetInstance(InstanceClass)).to.throw();
+        expect(() => i.GetInstance(InstanceClass)).toThrow();
     });
 
     it("Should set and return instance from cache", () => {
@@ -30,7 +29,7 @@ export const injectorTests = describe("Injector", () => {
         class InstanceClass { constructor() { /** */ } }
         const instance = new InstanceClass();
         i.SetInstance(instance);
-        expect(i.GetInstance(InstanceClass)).to.be.eq(instance);
+        expect(i.GetInstance(InstanceClass)).toBe(instance);
     });
 
     it("Should return from a parent injector if available", () => {
@@ -39,59 +38,59 @@ export const injectorTests = describe("Injector", () => {
         class InstanceClass { constructor() { /** */ } }
         const instance = new InstanceClass();
         Injector.Default.SetInstance(instance);
-        expect(i.GetInstance(InstanceClass)).to.be.eq(instance);
+        expect(i.GetInstance(InstanceClass)).toBe(instance);
         // tslint:disable-next-line:no-string-literal
-        expect(Injector.Default["cachedSingletons"].get(InstanceClass)).to.be.eq(instance);
+        expect(Injector.Default["cachedSingletons"].get(InstanceClass)).toBe(instance);
     });
 
     it("Should create instance on a parent injector if not available", () => {
         const i = new Injector();
         @Injectable()
         class InstanceClass { constructor() { /** */ } }
-        expect(i.GetInstance(InstanceClass)).to.be.instanceof(InstanceClass);
+        expect(i.GetInstance(InstanceClass)).toBeInstanceOf(InstanceClass);
         // tslint:disable-next-line:no-string-literal
-        expect(Injector.Default["cachedSingletons"].get(InstanceClass)).to.be.instanceof(InstanceClass);
+        expect(Injector.Default["cachedSingletons"].get(InstanceClass)).toBeInstanceOf(InstanceClass);
     });
 
     it("Should resolve parameters", () => {
         const i = new Injector();
 
-        class Injected1 {}
-        class Injected2 {}
+        class Injected1 { }
+        class Injected2 { }
 
         @Injectable()
         class InstanceClass { constructor(public injected1: Injected1, public injected2: Injected2) { /** */ } }
-        expect(i.GetInstance(InstanceClass)).to.be.instanceof(InstanceClass);
-        expect(i.GetInstance(InstanceClass).injected1).to.be.instanceof(Injected1);
-        expect(i.GetInstance(InstanceClass).injected2).to.be.instanceof(Injected2);
+        expect(i.GetInstance(InstanceClass)).toBeInstanceOf(InstanceClass);
+        expect(i.GetInstance(InstanceClass).injected1).toBeInstanceOf(Injected1);
+        expect(i.GetInstance(InstanceClass).injected2).toBeInstanceOf(Injected2);
     });
 
     it("Should resolve parameters recursively", () => {
         const i = new Injector();
 
-        class Injected1 {}
+        class Injected1 { }
         @Injectable()
-        class Injected2 { constructor( public injected1: Injected1) {  }}
+        class Injected2 { constructor(public injected1: Injected1) { } }
 
         @Injectable()
         class InstanceClass { constructor(public injected2: Injected2) { /** */ } }
-        expect(i.GetInstance(InstanceClass)).to.be.instanceof(InstanceClass);
-        expect(i.GetInstance(InstanceClass).injected2.injected1).to.be.instanceof(Injected1);
+        expect(i.GetInstance(InstanceClass)).toBeInstanceOf(InstanceClass);
+        expect(i.GetInstance(InstanceClass).injected2.injected1).toBeInstanceOf(Injected1);
     });
 
     it("Should be disposed", async () => {
-        await usingAsync(new Injector(),  async () => {/** */});
+        await usingAsync(new Injector(), async () => {/** */ });
     });
 
-    it("Should dispose cached entries on dispose and tolerate non-disposable ones", (done: MochaDone) => {
+    it("Should dispose cached entries on dispose and tolerate non-disposable ones", (done) => {
         class TestDisposable implements IDisposable {
             public dispose() {
                 done();
             }
         }
-        class TestInstance {}
+        class TestInstance { }
 
-        usingAsync(new Injector(),  async (i) => {
+        usingAsync(new Injector(), async (i) => {
             i.SetInstance(new TestDisposable());
             i.SetInstance(new TestInstance());
         });
