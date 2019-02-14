5f4ba1a15c30e9d6149e66b9bb70c7b89d2f0468
gallayl <gallay.lajos@gmail.com>
2019-02-14 17:03:05 +0100

    added placeholder websocket api test

--- packages/websocket-api/test/WebSocketApiTests.ts
+++ packages/websocket-api/test/WebSocketApiTests.ts
@@ -1,5 +1,12 @@
+import { Injector } from '@furystack/inject'
+import { WebSocketApi } from '../src/WebSocketApi'
+import { WebSocketApiConfiguration } from '../src/WebSocketApiConfiguration'
+
 describe('WebSocketApi', () => {
   it('Should be built', () => {
-    /** */
+    const i = new Injector()
+    i.setInstance({}, WebSocketApiConfiguration)
+    const instance = i.getInstance(WebSocketApi)
+    expect(instance).toBeInstanceOf(WebSocketApi)
   })
 })
