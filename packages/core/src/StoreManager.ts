736ea90f57d85a6aca8adbf069b17c3470d5da13
Gallay Lajos <gallay.lajos@gmail.com>
2019-04-16 09:40:42 +0200

    fix(core):storemanager import

--- packages/core/src/StoreManager.ts
+++ packages/core/src/StoreManager.ts
@@ -1,7 +1,7 @@
 import { Constructable, Injectable, Injector } from '@furystack/inject'
+import { ScopedLogger } from '@furystack/logging'
 import { Disposable } from '@sensenet/client-utils'
 import { DefaultFilter, IPhysicalStore } from './Models/IPhysicalStore'
-import { ScopedLogger } from '../../logging/dist'
 
 /**
  * Manager class for store instances
