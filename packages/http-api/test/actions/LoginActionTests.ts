f19931e53921ae9cf62051c2fb6852274d43c61b
Gallay Lajos <gallay.lajos@gmail.com>
2019-06-27 17:27:18 +0200

    test(http):Login action failure

--- packages/http-api/test/actions/LoginActionTests.ts
+++ packages/http-api/test/actions/LoginActionTests.ts
@@ -9,12 +9,33 @@ describe('LoginAction', () => {
   it('exec', done => {
     const testUser = { Name: 'Userke' }
     usingAsync(new Injector(), async i => {
-      i.setExplicitInstance({ cookieLogin: async () => testUser }, HttpUserContext)
+      i.setExplicitInstance({ cookieLogin: async () => testUser, authentication: { visitorUser: {} } }, HttpUserContext)
       i.setExplicitInstance({ readPostBody: async () => ({}) }, IncomingMessage)
       i.setExplicitInstance(
         {
           sendJson: (options: SendJsonOptions<any>) => {
             expect(options.json).toEqual(testUser)
+            done()
+          },
+        },
+        ServerResponse,
+      )
+      await usingAsync(i.getInstance(LoginAction), async c => {
+        await c.exec()
+      })
+    })
+  })
+
+  it('exec', done => {
+    const visitorUser = { Name: 'Visitorka' }
+    usingAsync(new Injector(), async i => {
+      i.setExplicitInstance({ cookieLogin: async () => visitorUser, authentication: { visitorUser } }, HttpUserContext)
+      i.setExplicitInstance({ readPostBody: async () => ({}) }, IncomingMessage)
+      i.setExplicitInstance(
+        {
+          sendJson: (options: SendJsonOptions<any>) => {
+            expect(options.statusCode).toBe(400)
+            expect(options.json).toEqual({ message: 'Login failed' })
             done()
           },
         },
