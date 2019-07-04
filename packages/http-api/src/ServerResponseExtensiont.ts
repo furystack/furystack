5e2df42c5d79d27462e29cc7a98a836f8aecaf95
Gallay Lajos <gallay.lajos@gmail.com>
2019-07-04 12:11:11 +0200

    test(http):sendActionResult

--- packages/http-api/src/ServerResponseExtensiont.ts
+++ packages/http-api/src/ServerResponseExtensiont.ts
@@ -15,8 +15,6 @@ export interface SendPlainTextOptions {
 declare module 'http' {
   export interface ServerResponse {
     sendActionResult: <T>(result: ActionResult<T>) => void
-    // sendJson: <T>(options: SendJsonOptions<T>) => void
-    // sendPlainText: (options: SendPlainTextOptions) => void
   }
 }
 
@@ -24,19 +22,3 @@ http.ServerResponse.prototype.sendActionResult = function<T>(options: ActionResu
   this.writeHead(options.statusCode, options.headers)
   this.end(options.chunk)
 }
-
-// http.ServerResponse.prototype.sendJson = function<T>(options: SendJsonOptions<T>) {
-//   this.writeHead(options.statusCode || 200, {
-//     'Content-Type': 'application/json',
-//     ...options.headers,
-//   })
-//   this.end(JSON.stringify(options.json))
-// }
-
-// http.ServerResponse.prototype.sendPlainText = function(options: SendPlainTextOptions) {
-//   this.writeHead(options.statusCode || 200, {
-//     'Content-Type': 'plain/text',
-//     ...options.headers,
-//   })
-//   this.end(options.text)
-// }
