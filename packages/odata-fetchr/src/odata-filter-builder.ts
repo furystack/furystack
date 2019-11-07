990ee1b28e4d0e69a145cc0f4746b0aa962b6706
Gallay Lajos <gallay.lajos@gmail.com>
2019-11-07 07:35:17 +0100

    fix(odata-fetchr):missing generic type

--- packages/odata-fetchr/src/odata-filter-builder.ts
+++ packages/odata-fetchr/src/odata-filter-builder.ts
@@ -14,7 +14,7 @@ export class ODataFilterBuilder<T> {
    * @returns The first ODataFilterExpression value for the ODataFilterBuilder
    */
   public static create<T>(): ODataFilterExpression<T> {
-    const builder = new ODataFilterBuilder()
+    const builder = new ODataFilterBuilder<T>()
     const firstSegment = new ODataFilterExpression(builder)
     return firstSegment
   }
