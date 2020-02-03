56029a58209d0ddde081f2db03d2aef8b5d9e355
Gallay Lajos <gallay.lajos@gmail.com>
2020-02-03 14:37:54 +0100

    fix(odata-fetchr):renamed useOdata to useOdataClient (fixes an extension method name collision)

--- packages/odata-fetchr/src/injector-extensions.ts
+++ packages/odata-fetchr/src/injector-extensions.ts
@@ -1,14 +1,14 @@
-import { Injector, Constructable } from '@furystack/inject'
 import { ODataServiceOptions, OdataService } from './odata-service'
+import { Injector, Constructable } from '@furystack/inject'
 
 declare module '@furystack/inject/dist/Injector' {
   interface Injector {
-    useOdata: (params: Omit<Omit<ODataServiceOptions, 'model'>, 'modelName'>) => Injector
+    useOdataClient: (params: Omit<Omit<ODataServiceOptions, 'model'>, 'modelName'>) => Injector
     getOdataServiceFor: <T>(model: Constructable<T>, modelName: string) => OdataService<T>
   }
 }
 
-Injector.prototype.useOdata = function(params) {
+Injector.prototype.useOdataClient = function(params) {
   this.setExplicitInstance({ ...params, model: Object.constructor }, ODataServiceOptions)
   this.getOdataServiceFor = function(model, modelName) {
     return new OdataService({ ...this.getInstance(ODataServiceOptions), model, modelName })
