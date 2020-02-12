299b9e2f609e3f2de7f34b007baab04ef8989bf1
Gallay Lajos <gallay.lajos@gmail.com>
2020-02-12 20:59:28 +0100

    test(mongo-store):stub

--- packages/mongodb-store/test/mongodb-store.tests.ts
+++ packages/mongodb-store/test/mongodb-store.tests.ts
@@ -1,8 +1,8 @@
 import { Injector } from '@furystack/inject'
 import { using } from '@furystack/utils'
 
-describe('TypeORM Store', () => {
-  it('Should be activated and disposed', () => {
+describe('MongoDB Store', () => {
+  it.todo('Should be activated and disposed', () => {
     using(new Injector(), i => {
       // ToDo
     })
