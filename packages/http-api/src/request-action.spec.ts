61b8a01f1a1db93c2fc5b86e46f73e67a55eccac
Lajos Gallay <gallay.lajos@gmail.com>
2020-02-23 15:33:08 +0100

    test(http-api):removed snapshots from default requests

--- packages/http-api/src/request-action.spec.ts
+++ packages/http-api/src/request-action.spec.ts
@@ -1,11 +1,43 @@
 import { JsonResult, PlainTextResult, XmlResult, EmptyResult, BypassResult } from './models/request-action'
 
 describe('RequestActions', () => {
-  it('Defaults should match the snapshots', () => {
-    expect(JsonResult({ foo: 1 })).toMatchSnapshot()
-    expect(PlainTextResult('alma')).toMatchSnapshot()
-    expect(XmlResult('alma')).toMatchSnapshot()
-    expect(EmptyResult()).toMatchSnapshot()
-    expect(BypassResult()).toMatchSnapshot()
+  it('should return the default JsonResult', () => {
+    expect(JsonResult({ foo: 1 })).toEqual({
+      chunk: '{"foo":1}',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      statusCode: 200,
+    })
+  })
+
+  it('should return the default PlainTextResult', () => {
+    expect(PlainTextResult('alma')).toEqual({
+      chunk: 'alma',
+      headers: {
+        'Content-Type': 'plain/text',
+      },
+      statusCode: 200,
+    })
+  })
+
+  it('should return the default XmlResult', () => {
+    expect(XmlResult('alma')).toEqual({
+      chunk: 'alma',
+      headers: {
+        'Content-Type': 'application/xml;charset=utf-8',
+      },
+      statusCode: 200,
+    })
+  })
+
+  it('should return the default EmptyResult', () => {
+    expect(EmptyResult()).toEqual({ headers: {}, statusCode: 200 })
+  })
+
+  it('should return the default BypassResult', () => {
+    expect(BypassResult()).toEqual({
+      chunk: 'BypassResult',
+    })
   })
 })
