8bc115999eaf6a16c816e0cccb3bac1eabd99de8
Gallay Lajos <gallay.lajos@gmail.com>
2020-03-18 08:43:20 +0100

    fix(rest-service): removed try/catch from authorize to avoid false forbidden errors

--- packages/rest-service/src/authorize.ts
+++ packages/rest-service/src/authorize.ts
@@ -9,25 +9,21 @@ export const Authorize = (...roles: string[]) => <T extends RequestActionOptions
 ): RequestAction<T> => {
   return async (options: RequestOptions<T['query'], T['body'], T['urlParams']>): Promise<ActionResult<T>> => {
     const userContext = options.injector.getInstance(HttpUserContext)
-    try {
-      const currentUser = await userContext.getCurrentUser()
-      const authorized = await userContext.isAuthorized(...roles)
-      if (!authorized) {
-        const { url } = options.injector.getInstance(IncomingMessage)
-        await sleepAsync(Math.random() * 1000)
-        options.injector.logger.warning({
-          scope: '@furystack/rest-service/@Authorize()',
-          message: `User '${currentUser.username}' has been tried to access to action '${url}' without the required roles.`,
-          data: {
-            user: currentUser,
-            requiredRoles: roles,
-          },
-        })
-        return JsonResult({ error: 'forbidden' }, 403) as any
-      }
-      return await action(options)
-    } catch (error) {
+    const currentUser = await userContext.getCurrentUser()
+    const authorized = await userContext.isAuthorized(...roles)
+    if (!authorized) {
+      const { url } = options.injector.getInstance(IncomingMessage)
+      await sleepAsync(Math.random() * 1000)
+      options.injector.logger.warning({
+        scope: '@furystack/rest-service/@Authorize()',
+        message: `User '${currentUser.username}' has been tried to access to action '${url}' without the required roles.`,
+        data: {
+          user: currentUser,
+          requiredRoles: roles,
+        },
+      })
       return JsonResult({ error: 'forbidden' }, 403) as any
     }
+    return await action(options)
   }
 }
