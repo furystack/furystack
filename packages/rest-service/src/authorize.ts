bdebc614bc7f5f02ae4199f8fa4c2a61e26f82f0
Gallay Lajos <gallay.lajos@gmail.com>
2020-03-18 09:10:14 +0100

    fix(authorize): re-added try/catch, fixed action execution outside of try/catch

--- packages/rest-service/src/authorize.ts
+++ packages/rest-service/src/authorize.ts
@@ -8,20 +8,24 @@ export const Authorize = (...roles: string[]) => <T extends RequestActionOptions
   action: RequestAction<T>,
 ): RequestAction<T> => {
   return async (options: RequestOptions<T['query'], T['body'], T['urlParams']>): Promise<ActionResult<T>> => {
-    const userContext = options.injector.getInstance(HttpUserContext)
-    const currentUser = await userContext.getCurrentUser()
-    const authorized = await userContext.isAuthorized(...roles)
-    if (!authorized) {
-      const { url } = options.injector.getInstance(IncomingMessage)
-      await sleepAsync(Math.random() * 1000)
-      options.injector.logger.warning({
-        scope: '@furystack/rest-service/@Authorize()',
-        message: `User '${currentUser.username}' has been tried to access to action '${url}' without the required roles.`,
-        data: {
-          user: currentUser,
-          requiredRoles: roles,
-        },
-      })
+    try {
+      const userContext = options.injector.getInstance(HttpUserContext)
+      const currentUser = await userContext.getCurrentUser()
+      const authorized = await userContext.isAuthorized(...roles)
+      if (!authorized) {
+        const { url } = options.injector.getInstance(IncomingMessage)
+        await sleepAsync(Math.random() * 1000)
+        options.injector.logger.warning({
+          scope: '@furystack/rest-service/@Authorize()',
+          message: `User '${currentUser.username}' has been tried to access to action '${url}' without the required roles.`,
+          data: {
+            user: currentUser,
+            requiredRoles: roles,
+          },
+        })
+        return JsonResult({ error: 'forbidden' }, 403) as any
+      }
+    } catch (error) {
       return JsonResult({ error: 'forbidden' }, 403) as any
     }
     return await action(options)
