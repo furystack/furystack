9ec7b34a4a5d7787eb4e4516c62f859bade70d75
Gallay Lajos <gallay.lajos@gmail.com>
2020-04-29 13:37:07 +0200

    fix(websocket): cleaned up unused import

--- packages/websocket-api/src/actions/whoami.spec.ts
+++ packages/websocket-api/src/actions/whoami.spec.ts
@@ -2,7 +2,7 @@ import { WhoAmI } from './whoami'
 import { HttpUserContext } from '@furystack/rest-service'
 import ws from 'ws'
 import { usingAsync } from '@furystack/utils'
-import { IncomingMessage, ServerResponse } from 'http'
+import { IncomingMessage } from 'http'
 
 describe('Whoami action', () => {
   const currentUser = { username: 'testuser' }
