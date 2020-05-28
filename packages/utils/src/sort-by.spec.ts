0ef74aed9ceeebb1a96aff7477b9942a2c10c1f0
Gallay Lajos <gallay.lajos@gmail.com>
2020-05-28 16:32:27 +0200

    feat(utils): Array sort by

---
+++ packages/utils/src/sort-by.spec.ts
@@ -0,0 +1,16 @@
+import './sort-by'
+describe('sortBy', () => {
+  it('Should sort by ascending by default', () => {
+    const arr = [{ v: 5 }, { v: 4 }, { v: 3 }]
+    expect(arr.sortBy('v')).toEqual([{ v: 3 }, { v: 4 }, { v: 5 }])
+  })
+  it('Should sort by ascending', () => {
+    const arr = [{ v: 5 }, { v: 4 }, { v: 3 }]
+    expect(arr.sortBy('v', 'asc')).toEqual([{ v: 3 }, { v: 4 }, { v: 5 }])
+  })
+
+  it('Should sort by descending', () => {
+    const arr = [{ v: 3 }, { v: 4 }, { v: 5 }]
+    expect(arr.sortBy('v', 'desc')).toEqual([{ v: 5 }, { v: 4 }, { v: 3 }])
+  })
+})
---
+++ packages/utils/src/sort-by.ts
@@ -0,0 +1,27 @@
+declare global {
+  /**
+   * Defines an array of elements
+   */
+  export interface Array<T> {
+    /**
+     * Returns a promise with a new array of elements that meets the specified async callback
+     */
+    sortBy: (field: keyof T, direction?: 'asc' | 'desc') => T[]
+  }
+}
+
+export const compareBy = <T, K extends keyof T>(entity1: T, entity2: T, field: K, direction: 'asc' | 'desc') => {
+  const a = direction === 'asc' ? entity1 : entity2
+  const b = direction === 'asc' ? entity2 : entity1
+  if (a[field] < b[field]) {
+    return -1
+  }
+  if (a[field] > b[field]) {
+    return 1
+  }
+  return 0
+}
+
+Array.prototype.sortBy = function (key, direction = 'asc') {
+  return this.sort((a, b) => compareBy(a, b, key, direction))
+}
