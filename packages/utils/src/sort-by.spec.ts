f7dde9c8430e628f81a18f6ef6d618eed11b55f3
Gallay Lajos <gallay.lajos@gmail.com>
2020-05-28 16:34:25 +0200

    test(utils):sort-by tests

--- packages/utils/src/sort-by.spec.ts
+++ packages/utils/src/sort-by.spec.ts
@@ -1,9 +1,10 @@
 import './sort-by'
 describe('sortBy', () => {
   it('Should sort by ascending by default', () => {
-    const arr = [{ v: 5 }, { v: 4 }, { v: 3 }]
+    const arr = [{ v: 5 }, { v: 3 }, { v: 4 }]
     expect(arr.sortBy('v')).toEqual([{ v: 3 }, { v: 4 }, { v: 5 }])
   })
+
   it('Should sort by ascending', () => {
     const arr = [{ v: 5 }, { v: 4 }, { v: 3 }]
     expect(arr.sortBy('v', 'asc')).toEqual([{ v: 3 }, { v: 4 }, { v: 5 }])
@@ -12,5 +13,10 @@ describe('sortBy', () => {
   it('Should sort by descending', () => {
     const arr = [{ v: 3 }, { v: 4 }, { v: 5 }]
     expect(arr.sortBy('v', 'desc')).toEqual([{ v: 5 }, { v: 4 }, { v: 3 }])
+  })
+
+  it('Should handle equalities', () => {
+    const arr = [{ v: 3 }, { v: 4 }, { v: 4 }]
+    expect(arr.sortBy('v', 'desc')).toEqual([{ v: 4 }, { v: 4 }, { v: 3 }])
   })
 })
