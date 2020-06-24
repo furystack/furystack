0a59db43d5d0236e7fadf6a80a51b3ad075cc1eb
Gallay Lajos <gallay.lajos@gmail.com>
2020-06-24 16:21:20 +0200

    feat(rest-service):log only server errors

--- packages/rest-service/src/actions/error-action.ts
+++ packages/rest-service/src/actions/error-action.ts
@@ -12,15 +12,17 @@ export const ErrorAction: RequestAction<{
   result: { message: string; url?: string; stack?: string }
 }> = async ({ injector, getBody, request }) => {
   const body = await getBody()
-  injector.logger.warning({
-    scope: '@furystack/rest-service',
-    message: `An action throwed error from '${request.url}'.`,
-    data: {
-      error: body,
-    },
-  })
-
   const errorCode = body instanceof RequestError ? body.responseCode : body instanceof AuthorizationError ? 403 : 500
+
+  if (errorCode >= 500 && errorCode <= 511) {
+    injector.logger.warning({
+      scope: '@furystack/rest-service',
+      message: `An action throwed error from '${request.url}'.`,
+      data: {
+        error: body,
+      },
+    })
+  }
 
   return JsonResult({ message: body.message, url: request.url, stack: body.stack }, errorCode)
 }
