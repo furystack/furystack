964912e25baa28edf203d97fca3185ee9c842a2a
Gallay Lajos <gallay.lajos@gmail.com>
2021-01-05 10:45:05 +0100

    test(filesystem-store): tick and reload tests

--- packages/filesystem-store/src/filesystem-store.spec.ts
+++ packages/filesystem-store/src/filesystem-store.spec.ts
@@ -2,6 +2,7 @@ import { promises, existsSync } from 'fs'
 import { FileSystemStore } from './filesystem-store'
 import { TestClass, createStoreTest } from '@furystack/core/dist/create-physical-store-tests'
 import { v4 } from 'uuid'
+import { sleepAsync } from '@furystack/utils'
 
 describe('FileSystemStore', () => {
   const storeNames: string[] = []
@@ -14,6 +15,48 @@ describe('FileSystemStore', () => {
   createStoreTest({
     createStore,
     typeName: 'FileStore',
+  })
+
+  it('Should save data on tick', async () => {
+    const fileName = `filestore-test-${v4()}.json`
+    storeNames.push(fileName)
+    const store = new FileSystemStore({ model: TestClass, fileName, primaryKey: 'id', tickMs: 500 })
+    store.saveChanges = jest.fn(store.saveChanges.bind(store))
+
+    await store.add({
+      id: 1,
+      dateValue: new Date(),
+      stringValue1: 'alma',
+      stringValue2: 'korte',
+      booleanValue: false,
+      numberValue1: 1,
+      numberValue2: 2,
+    })
+    await sleepAsync(501)
+    expect(store.saveChanges).toHaveBeenCalled()
+
+    await store.dispose()
+  })
+
+  it('Should reload data from disk', async () => {
+    const fileName = `filestore-test-${v4()}.json`
+    storeNames.push(fileName)
+    const store = new FileSystemStore({ model: TestClass, fileName, primaryKey: 'id', tickMs: 500 })
+    await store.add({
+      id: 1,
+      dateValue: new Date(),
+      stringValue1: 'alma',
+      stringValue2: 'korte',
+      booleanValue: false,
+      numberValue1: 1,
+      numberValue2: 2,
+    })
+    await store.saveChanges()
+    await store.remove(1)
+    await store.reloadData()
+    const count = await store.count()
+    expect(count).toBe(1)
+    await store.dispose()
   })
 
   afterAll(async () => {
