c401f2f27fbcf40e1608ef9ba8d1af456b486516
Lajos <gallayl@users.noreply.github.com>
2021-08-25 13:16:44 +0200

    more roboust server manager dispose (#153)

--- packages/rest-service/src/server-manager.ts
+++ packages/rest-service/src/server-manager.ts
@@ -38,45 +38,50 @@ export class ServerManager implements Disposable {
   }
 
   public async dispose() {
-    await this.listenLock.acquire()
-    this.openedSockets.forEach((s) => s.destroy())
-    await Promise.allSettled(
-      [...this.servers.values()].map(
-        (s) =>
-          new Promise<void>((resolve, reject) => {
-            s.server.close((err) => (err ? reject(err) : resolve()))
-            s.server.off('connection', this.onConnection)
-          }),
-      ),
-    )
-    this.servers.clear()
-    this.listenLock.release()
+    try {
+      await this.listenLock.waitFor(5000)
+    } finally {
+      this.openedSockets.forEach((s) => s.destroy())
+      await Promise.allSettled(
+        [...this.servers.values()].map(
+          (s) =>
+            new Promise<void>((resolve, reject) => {
+              s.server.close((err) => (err ? reject(err) : resolve()))
+              s.server.off('connection', this.onConnection)
+            }),
+        ),
+      )
+      this.servers.clear()
+      this.listenLock.release()
+    }
   }
 
   public async getOrCreate(options: ServerOptions): Promise<ServerRecord> {
     const url = this.getHostUrl(options)
     if (!this.servers.has(url)) {
       await this.listenLock.acquire()
-      if (!this.servers.has(url)) {
-        await new Promise<void>((resolve, reject) => {
-          const apis: ServerRecord['apis'] = []
-          const server = createServer((req, res) => {
-            const apiMatch = apis.find((api) => api.shouldExec({ req, res }))
-            if (apiMatch) {
-              apiMatch.onRequest({ req, res })
-            } else {
-              res.destroy()
-            }
+      try {
+        if (!this.servers.has(url)) {
+          await new Promise<void>((resolve, reject) => {
+            const apis: ServerRecord['apis'] = []
+            const server = createServer((req, res) => {
+              const apiMatch = apis.find((api) => api.shouldExec({ req, res }))
+              if (apiMatch) {
+                apiMatch.onRequest({ req, res })
+              } else {
+                res.destroy()
+              }
+            })
+            server.on('connection', this.onConnection)
+            server.on('listening', () => resolve())
+            server.on('error', (err) => reject(err))
+            server.listen(options.port, options.hostName)
+            this.servers.set(url, { server, apis })
           })
-          server.on('connection', this.onConnection)
-          server
-            .listen(options.port, options.hostName)
-            .on('listening', () => resolve())
-            .on('error', (err) => reject(err))
-          this.servers.set(url, { server, apis })
-        })
+        }
+      } finally {
+        this.listenLock.release()
       }
-      this.listenLock.release()
     }
     return this.servers.get(url) as ServerRecord
   }
