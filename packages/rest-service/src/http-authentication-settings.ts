f81b3af908ea8780f13d3a632f06a205ea243b9b
Gallay Lajos <gallay.lajos@gmail.com>
2022-03-26 11:01:31 +0100

    lint: fixed prettier

--- packages/rest-service/src/http-authentication-settings.ts
+++ packages/rest-service/src/http-authentication-settings.ts
@@ -9,9 +9,8 @@ import { DefaultSession } from './models/default-session'
 export class HttpAuthenticationSettings<TUser extends User, TSession extends DefaultSession> {
   public model: Constructable<TUser> = User as Constructable<TUser>
 
-  public getUserStore: (storeManager: StoreManager) => PhysicalStore<TUser, keyof TUser> = (
-    sm,
-  ) => sm.getStoreFor<TUser, keyof TUser>(User as any, 'username')
+  public getUserStore: (storeManager: StoreManager) => PhysicalStore<TUser, keyof TUser> = (sm) =>
+    sm.getStoreFor<TUser, keyof TUser>(User as any, 'username')
 
   public getSessionStore: (storeManager: StoreManager) => PhysicalStore<TSession, keyof TSession> = (sm) =>
     sm.getStoreFor(DefaultSession, 'sessionId') as unknown as PhysicalStore<TSession, keyof TSession>
