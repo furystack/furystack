6cbda72e86728c5bbfae6a62d8cd4ea5479c19eb
Gallay Lajos <gallay.lajos@gmail.com>
2022-05-30 09:31:51 +0200

    added screen shot uploads

--- packages/shades-showcase-app/playwright.config.ts
+++ packages/shades-showcase-app/playwright.config.ts
@@ -5,7 +5,7 @@ const config: PlaywrightTestConfig = {
   testDir: 'e2e',
   fullyParallel: true,
   retries: process.env.CI ? 2 : 0,
-  reporter: [['list'], ['junit', { outputFile: 'test-results.xml' }]],
+  reporter: [['list'], ['junit', { outputFile: 'test-results.xml', suiteName: 'Shades Showcase App' }]],
   expect: {
     toHaveScreenshot: {
       maxDiffPixelRatio: 0.05,
