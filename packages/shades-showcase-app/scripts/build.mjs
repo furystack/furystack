a7043058aab07eac0cc2dbf629bb68932e847a92
Gallay Lajos <gallay.lajos@gmail.com>
2022-05-30 21:32:59 +0200

    try serve in the background

--- packages/shades-showcase-app/scripts/build.mjs
+++ packages/shades-showcase-app/scripts/build.mjs
@@ -4,6 +4,7 @@ import { pnpPlugin } from '@yarnpkg/esbuild-plugin-pnp'
 build({
   plugins: [pnpPlugin()],
   entryPoints: ['./src/index.tsx'],
+  jsxFactory: 'createComponent',
   outdir: 'bundle/js',
   bundle: true,
   minify: true,
