45900ef0047c881ce898ba560d3f3f624a0a7cb6
Gallay Lajos <gallay.lajos@gmail.com>
2022-06-23 12:13:07 +0200

    improved build script

--- packages/shades-showcase-app/scripts/build.mjs
+++ packages/shades-showcase-app/scripts/build.mjs
@@ -8,8 +8,17 @@ build({
   outdir: 'bundle/js',
   bundle: true,
   minify: true,
+  logLevel: 'debug',
   sourcemap: true,
   splitting: true,
   platform: 'browser',
   format: 'esm',
 })
+  .then((result) => {
+    console.log('Build successful', result)
+    process.exit(0)
+  })
+  .catch((error) => {
+    console.error('Build failed', error)
+    process.exit(1)
+  })
