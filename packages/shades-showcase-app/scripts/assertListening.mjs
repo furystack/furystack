532a2093d8f7d2e9a10b9e94f7073a6adbf5ede1
Gallay Lajos <gallay.lajos@gmail.com>
2022-06-23 13:05:43 +0200

    improved assertListening

--- packages/shades-showcase-app/scripts/assertListening.mjs
+++ packages/shades-showcase-app/scripts/assertListening.mjs
@@ -1,9 +1,15 @@
-fetch('http://localhost:8080').then((response) => {
-  if (response.ok) {
+const fail = (reason) => {
+  console.log(`Server is not listening`, reason)
+  process.exit(1)
+}
+
+fetch('http://127.0.0.1:8080/')
+  .then((response) => {
+    if (!response.ok) {
+      fail({ status: response.status, statusText: response.statusText, response })
+    }
+  })
+  .then(() => {
     console.log('Server is listening')
-    process.exit(0)
-  } else {
-    console.log(`Server is not listening, status is ${response.status}`)
-    process.exit(1)
-  }
-})
+  })
+  .catch(fail)
