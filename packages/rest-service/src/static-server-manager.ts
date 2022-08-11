d27599b69b86e14bb620587f362608dd464065b7
Gallay Lajos <gallay.lajos@gmail.com>
2022-08-11 17:27:13 +0200

    try debug 404 in CI

--- packages/rest-service/src/static-server-manager.ts
+++ packages/rest-service/src/static-server-manager.ts
@@ -51,6 +51,7 @@ export class StaticServerManager {
         if (fallback) {
           await this.sendFile(join(path, fallback), res)
         } else {
+          console.error((error as any).message)
           res.writeHead(404, { 'Content-Type': 'text/plain' })
           res.end('Not found')
         }
