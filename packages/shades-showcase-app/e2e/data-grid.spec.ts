10d55a278c377d54d83f253708af42e47094c3ca
Gallay Lajos <gallay.lajos@gmail.com>
2022-08-19 10:50:45 +0200

    added grid tests

--- packages/shades-showcase-app/e2e/data-grid.spec.ts
+++ packages/shades-showcase-app/e2e/data-grid.spec.ts
@@ -1,4 +1,5 @@
 import { Locator, Page, test } from '@playwright/test'
+import { sleepAsync } from '@furystack/utils'
 import { pages } from './pages'
 
 const getGrid = async (page: Page) => {
@@ -10,18 +11,30 @@ test.describe('Data Grid component', () => {
   let focusedEntry!: Locator
   let selectionCount!: Locator
 
-  const expectRowHasFocus = async (page: Page, rowNumber: number) => {
-    const row = await page.locator(`shades-data-grid-row:nth-child(${rowNumber})`)
+  const expectRowHasFocus = async (rowNumber: number) => {
+    const row = await grid.locator(`shades-data-grid-row:nth-child(${rowNumber})`)
     const rowTextCell = await row.locator('td:nth-child(2)')
     test.expect(await focusedEntry.inputValue()).toBe(await rowTextCell.textContent())
   }
 
-  const expectSelectionCound = async (page: Page, count: number) => {
+  const expectRowIsSelected = async (rowNumber: number) => {
+    const row = await grid.locator(`shades-data-grid-row:nth-child(${rowNumber})`)
+    const checkbox = await row.locator('td:nth-child(1) input[type=checkbox]')
+    test.expect(await checkbox.isChecked()).toBe(true)
+  }
+
+  const expectRowIsUnselected = async (rowNumber: number) => {
+    const row = await grid.locator(`shades-data-grid-row:nth-child(${rowNumber})`)
+    const checkbox = await row.locator('td:nth-child(1) input[type=checkbox]')
+    test.expect(await checkbox.isChecked()).toBe(false)
+  }
+
+  const expectSelectionCount = async (count: number) => {
     test.expect(await selectionCount.inputValue()).toBe(count.toString())
   }
 
-  const clickOnRow = async (page: Page, rowNumber: number) => {
-    const row = await page.locator(`shades-data-grid-row:nth-child(${rowNumber})`)
+  const clickOnRow = async (rowNumber: number) => {
+    const row = await grid.locator(`shades-data-grid-row:nth-child(${rowNumber})`)
     await row.click()
   }
 
@@ -33,22 +46,124 @@ test.describe('Data Grid component', () => {
   })
 
   test.describe('Focus', async () => {
-    test('With mouse click', async ({ page }) => {
+    test('With mouse click', async () => {
       const numbers = new Array(10).fill(Math.round(Math.random() * 100))
       for (const no of numbers) {
-        await clickOnRow(page, no)
-        await expectRowHasFocus(page, no)
-        await expectSelectionCound(page, 0)
+        await clickOnRow(no)
+        await expectRowHasFocus(no)
+        await expectSelectionCount(0)
       }
     })
 
     test('With keyboard', async ({ page }) => {
-      await clickOnRow(page, 1)
-      await expectRowHasFocus(page, 1)
+      await clickOnRow(1)
+      await expectRowHasFocus(1)
       await page.keyboard.press('ArrowDown')
-      await expectRowHasFocus(page, 2)
+      await expectRowHasFocus(2)
       await page.keyboard.press('ArrowUp')
-      await expectRowHasFocus(page, 1)
+      await expectRowHasFocus(1)
+    })
+  })
+
+  test.describe('Selection', async () => {
+    test.describe('Keyboard shortcuts', () => {
+      test('space should invert selection and keep focus', async ({ page }) => {
+        await clickOnRow(1)
+
+        // select row 1
+        await expectRowHasFocus(1)
+        await page.keyboard.press(' ')
+        await expectRowHasFocus(1)
+        await expectSelectionCount(1)
+        await expectRowIsSelected(1)
+
+        // select row 2
+        await page.keyboard.press('ArrowDown')
+        await page.keyboard.press(' ')
+        await expectRowHasFocus(2)
+        await expectSelectionCount(2)
+        await expectRowIsSelected(1)
+        await expectRowIsSelected(2)
+
+        // select row 3
+        await page.keyboard.press('ArrowDown')
+        await page.keyboard.press(' ')
+        await expectRowHasFocus(3)
+        await expectSelectionCount(3)
+        await expectRowIsSelected(1)
+        await expectRowIsSelected(2)
+        await expectRowIsSelected(3)
+
+        // deselect row 2
+        await page.keyboard.press('ArrowUp')
+        await page.keyboard.press(' ')
+        await expectRowHasFocus(2)
+        await expectSelectionCount(2)
+        await expectRowIsSelected(1)
+        await expectRowIsUnselected(2)
+        await expectRowIsSelected(3)
+      })
+
+      test('insert should invert selection and move focus down', async ({ page }) => {
+        await clickOnRow(1)
+
+        // select row 1, focus on 2
+        await page.keyboard.press('Insert')
+        await expectRowHasFocus(2)
+        await expectRowIsSelected(1)
+        await expectSelectionCount(1)
+
+        // select row 2, focus on 3
+        await page.keyboard.press('Insert')
+        await expectRowHasFocus(3)
+        await expectRowIsSelected(2)
+        await expectSelectionCount(2)
+
+        await page.keyboard.press('ArrowUp')
+        await expectRowHasFocus(2)
+        await page.keyboard.press('Insert')
+        await expectRowIsUnselected(2)
+        await expectSelectionCount(1)
+        await expectRowHasFocus(3)
+      })
+
+      test('plus should select all rows', async ({ page }) => {
+        await clickOnRow(1)
+        await page.keyboard.press('+', {
+          delay: 10,
+        })
+        await expectSelectionCount(100)
+      })
+
+      test('minus should deselect all rows', async ({ page }) => {
+        await clickOnRow(1)
+        await page.keyboard.press('Insert')
+        await page.keyboard.press('Insert')
+        await page.keyboard.press('Insert')
+        await expectSelectionCount(3)
+        await page.keyboard.press('-')
+        await expectSelectionCount(0)
+      })
+
+      test('star should invert selection', async ({ page }) => {
+        await clickOnRow(1)
+        await page.keyboard.press('Insert')
+        await page.keyboard.press('Insert')
+        await page.keyboard.press('Insert')
+        await expectSelectionCount(3)
+        await page.keyboard.press('*')
+        await expectSelectionCount(97)
+      })
+    })
+  })
+
+  test.describe('Gestures', async () => {
+    test('CTRL+click should toggle selection', async ({ page }) => {
+      /** TODO */
+    })
+
+    test('SHIFT+click should select range', async ({ page }) => {
+      /** TODO */
     })
   })
 })
