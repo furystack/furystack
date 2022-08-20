b536fac78688c1aa29df002699b8429493209253
Gallay Lajos <gallay.lajos@gmail.com>
2022-08-20 22:26:11 +0200

    feat(animation): optional elements

--- packages/shades-common-components/src/components/animations.ts
+++ packages/shades-common-components/src/components/animations.ts
@@ -1,33 +1,35 @@
 import { promisifyAnimation } from '../utils/promisify-animation'
 
 export const animations = {
-  showSlide: async (options: { element: Element }) => {
-    await promisifyAnimation(
-      options.element,
-      [
-        { transform: 'translate(-350px, 0)scale(0)', opacity: 0 },
-        { transform: 'translate(0, 0)scale(1)', opacity: 1 },
-      ],
-      {
-        duration: 500,
-        easing: 'cubic-bezier(0.215, 0.610, 0.355, 1.000)',
-        fill: 'forwards',
-      },
-    )
+  showSlide: async (element?: Element | null) => {
+    element &&
+      (await promisifyAnimation(
+        element,
+        [
+          { transform: 'translate(-350px, 0)scale(0)', opacity: 0 },
+          { transform: 'translate(0, 0)scale(1)', opacity: 1 },
+        ],
+        {
+          duration: 500,
+          easing: 'cubic-bezier(0.215, 0.610, 0.355, 1.000)',
+          fill: 'forwards',
+        },
+      ))
   },
-  hideSlide: async (options: { element: Element }) => {
-    await promisifyAnimation(
-      options.element,
-      [
-        { transform: 'translate(0, 0)scale(1)', opacity: 1 },
-        { transform: 'translate(-350px, 0)scale(0)', opacity: 0 },
-      ],
-      {
-        duration: 500,
-        easing: 'cubic-bezier(0.215, 0.610, 0.355, 1.000)',
-        fill: 'forwards',
-      },
-    )
+  hideSlide: async (element?: Element | null) => {
+    element &&
+      (await promisifyAnimation(
+        element,
+        [
+          { transform: 'translate(0, 0)scale(1)', opacity: 1 },
+          { transform: 'translate(-350px, 0)scale(0)', opacity: 0 },
+        ],
+        {
+          duration: 500,
+          easing: 'cubic-bezier(0.215, 0.610, 0.355, 1.000)',
+          fill: 'forwards',
+        },
+      ))
   },
   fadeOut: async (element?: Element | null) => {
     element &&
