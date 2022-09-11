88f2a1737a75cd355180655e90e27293a88c04c9
Gallay <gallay.lajos@gmail.com>
2022-09-11 13:07:38 +0200

    reverted screen service changes, updated docs

--- packages/shades/src/services/screen-service.ts
+++ packages/shades/src/services/screen-service.ts
@@ -7,10 +7,16 @@ export type ScreenSize = typeof ScreenSizes[number]
 
 export type Breakpoint = { name: ScreenSize; minSize: number; maxSize?: number }
 
+/**
+ * Service for handling screen size changes
+ */
 @Injectable({ lifetime: 'singleton' })
 export class ScreenService implements Disposable {
   private getOrientation = () => (window.matchMedia?.('(orientation:landscape').matches ? 'landscape' : 'portrait')
 
+  /**
+   * The definitions of the breakpoints
+   */
   public readonly breakpoints: { [K in ScreenSize]: { minSize: number } } = {
     xl: { minSize: 1920 },
     lg: { minSize: 1280 },
@@ -23,27 +29,28 @@ export class ScreenService implements Disposable {
     window.removeEventListener('resize', this.onResizeListener)
   }
 
+  /**
+   * Observers for the current screen size. Will refresh on resize
+   */
   public readonly screenSize: {
-    atLeast: { [K in ScreenSize]: ObservableValue<{ isMatched: boolean; width: number; height: number }> }
+    atLeast: { [K in ScreenSize]: ObservableValue<boolean> }
   } = {
     atLeast: {
-      xs: new ObservableValue<{ isMatched: boolean; width: number; height: number }>(this.screenSizeAtLeast('xs')),
-      sm: new ObservableValue<{ isMatched: boolean; width: number; height: number }>(this.screenSizeAtLeast('sm')),
-      md: new ObservableValue<{ isMatched: boolean; width: number; height: number }>(this.screenSizeAtLeast('md')),
-      lg: new ObservableValue<{ isMatched: boolean; width: number; height: number }>(this.screenSizeAtLeast('lg')),
-      xl: new ObservableValue<{ isMatched: boolean; width: number; height: number }>(this.screenSizeAtLeast('xl')),
+      xs: new ObservableValue<boolean>(this.screenSizeAtLeast('xs')),
+      sm: new ObservableValue<boolean>(this.screenSizeAtLeast('sm')),
+      md: new ObservableValue<boolean>(this.screenSizeAtLeast('md')),
+      lg: new ObservableValue<boolean>(this.screenSizeAtLeast('lg')),
+      xl: new ObservableValue<boolean>(this.screenSizeAtLeast('xl')),
     },
   }
 
   private screenSizeAtLeast(size: ScreenSize) {
-    const isMatched = window.innerWidth >= this.breakpoints[size].minSize
-    return {
-      isMatched,
-      width: window.innerWidth,
-      height: window.innerHeight,
-    }
+    return window.innerWidth >= this.breakpoints[size].minSize
   }
 
+  /**
+   * Observable value for tracking the screen orientation
+   */
   public orientation = new ObservableValue<'landscape' | 'portrait'>(this.getOrientation())
 
   private onResizeListener = () => {
