5b5cd71506bef761267b7ed067fd635921dffb10
Gallay Lajos <gallay.lajos@gmail.com>
2022-10-15 18:36:49 +0200

    vitest to package scope

---
+++ packages/rest/vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: 'c8',
+      reporter: ['text', 'json', 'html', 'cobertura'],
+    },
+    reporters: ['default', 'junit'],
+    outputFile: 'coverage/junit.xml',
+    include: ['src/**/*.{test,spec,tests,specs}.{ts,tsx}'],
+  },
+  build: {
+    commonjsOptions: {
+      strictRequires: 'debug',
+    },
+  },
+})
