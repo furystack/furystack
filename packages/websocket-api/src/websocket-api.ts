97a0c528b9c4c48b5ae09b9e942861d7f609d85c
Gallay Lajos <gallay.lajos@gmail.com>
2022-10-15 18:52:27 +0200

    fix websocket api import

--- packages/websocket-api/src/websocket-api.ts
+++ packages/websocket-api/src/websocket-api.ts
@@ -5,7 +5,7 @@ import { ServerManager } from '@furystack/rest-service'
 import { Injectable, Injected, Injector } from '@furystack/inject'
 import type { Disposable } from '@furystack/utils'
 import type { Data } from 'ws'
-import ws from 'ws'
+import ws, { WebSocketServer } from 'ws'
 import { WebSocketApiSettings } from './websocket-api-settings.js'
 import type { WebSocketAction } from './models/websocket-action.js'
 import { AggregatedError, IdentityContext } from '@furystack/core'
@@ -16,7 +16,7 @@ import { WebsocketUserContext } from './websocket-user-context.js'
  */
 @Injectable({ lifetime: 'scoped' })
 export class WebSocketApi implements Disposable {
-  public readonly socket = new ws.WebSocketServer({ noServer: true })
+  public readonly socket = new WebSocketServer({ noServer: true })
 
   private clients = new Map<ws, { injector: Injector; ws: ws; message: IncomingMessage }>()
 
