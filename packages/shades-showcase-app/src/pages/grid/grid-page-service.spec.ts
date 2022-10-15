9fc39cb09d406206457e5c4f7cd9d77472db3f79
Gallay Lajos <gallay.lajos@gmail.com>
2022-10-15 19:51:57 +0200

    removed pnp plugin, fixed tests

--- packages/shades-showcase-app/src/pages/grid/grid-page-service.spec.ts
+++ packages/shades-showcase-app/src/pages/grid/grid-page-service.spec.ts
@@ -6,11 +6,7 @@ describe('GridPageService', () => {
   it('should be populated with a default state', async () => {
     await usingAsync(new Injector(), async (injector) => {
       const instance = await injector.getInstance(GridPageService)
-      expect(instance['isInitialized']).toBe(false)
-
       await instance.init()
-      expect(instance['isInitialized']).toBe(true)
-
       await instance.collectionService.getEntries({})
       expect(instance.collectionService.data.getValue().count).toBe(100)
     })
