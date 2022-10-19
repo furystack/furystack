a2591cfe98303715be1cda5f12ea4a5fbd3be349
Gallay Lajos <gallay.lajos@gmail.com>
2022-10-19 22:21:36 +0200

    c8 to istanbul

--- packages/shades-nipple/vitest.config.ts
+++ packages/shades-nipple/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
-      provider: 'c8',
+      provider: 'istanbul',
       reporter: ['text', 'json', 'html', 'cobertura', 'lcov'],
     },
     reporters: ['default', 'junit'],
