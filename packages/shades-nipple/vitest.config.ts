e3c0840e7ccedc3c8cc7f2ca7aa82fd8f0727835
Gallay Lajos <gallay.lajos@gmail.com>
2022-10-19 20:11:50 +0200

    add lcov reports

--- packages/shades-nipple/vitest.config.ts
+++ packages/shades-nipple/vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     coverage: {
       provider: 'c8',
-      reporter: ['text', 'json', 'html', 'cobertura'],
+      reporter: ['text', 'json', 'html', 'cobertura', 'lcov'],
     },
     reporters: ['default', 'junit'],
     outputFile: 'coverage/junit.xml',
