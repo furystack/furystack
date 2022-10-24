b8ea16bd79bf5030dcfd0b7e0b2d097016bbbcc6
Gallay Lajos <gallay.lajos@gmail.com>
2022-10-24 17:23:06 +0200

    reverted to localhost

--- packages/shades-showcase-app/e2e/pages.ts
+++ packages/shades-showcase-app/e2e/pages.ts
@@ -1,38 +1,38 @@
 export const pages = {
   buttons: {
     name: 'Buttons',
-    url: 'http://127.0.0.1:8080/buttons',
+    url: 'http://localhost:8080/buttons',
   },
   inputs: {
     name: 'Inputs',
-    url: 'http://127.0.0.1:8080/inputs',
+    url: 'http://localhost:8080/inputs',
   },
   grid: {
     name: 'Grid',
-    url: 'http://127.0.0.1:8080/grid',
+    url: 'http://localhost:8080/grid',
   },
   nipple: {
     name: 'Nipple',
-    url: 'http://127.0.0.1:8080/nipple',
+    url: 'http://localhost:8080/nipple',
   },
   lottie: {
     name: 'Lottie',
-    url: 'http://127.0.0.1:8080/lottie',
+    url: 'http://localhost:8080/lottie',
   },
   monaco: {
     name: 'Monaco',
-    url: 'http://127.0.0.1:8080/monaco',
+    url: 'http://localhost:8080/monaco',
   },
   wizard: {
     name: 'Wizard',
-    url: 'http://127.0.0.1:8080/wizard',
+    url: 'http://localhost:8080/wizard',
   },
   notys: {
     name: 'Notys',
-    url: 'http://127.0.0.1:8080/notys',
+    url: 'http://localhost:8080/notys',
   },
   tabs: {
     name: 'Tabs',
-    url: 'http://127.0.0.1:8080/tabs',
+    url: 'http://localhost:8080/tabs',
   },
 }
