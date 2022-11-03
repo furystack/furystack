7886a846da38d7c7d4a22529617f39929060d46e
Gallay Lajos <gallay.lajos@gmail.com>
2022-11-03 13:15:30 +0100

    tryfix selection count assert

--- packages/shades-showcase-app/e2e/data-grid.spec.ts
+++ packages/shades-showcase-app/e2e/data-grid.spec.ts
@@ -29,8 +29,8 @@ test.describe('Data Grid component', () => {
 
   const expectSelectionCount = async (page: Page, count: number) => {
     await page.locator(`[data-selectionLength="${count}"`)
-    const selectionCountValue = await page.locator('[name="selectionCount"]').inputValue()
-    await expect(selectionCountValue).toBe(count.toString())
+    const selectionCountInput = await page.locator('[name="selectionCount"]')
+    await expect(await selectionCountInput.inputValue()).toBe(count.toString())
   }
 
   const clickOnRow = async (page: Page, rowNumber: number, modifiers?: Array<'Alt' | 'Control' | 'Meta' | 'Shift'>) => {
