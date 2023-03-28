b76a57b651cc67f6044c959bde1e154365dbe51a
Lajos <gallayl@users.noreply.github.com>
2023-03-28 15:52:17 +0200

    Feat/cache (#282)
    * feat(cache): Cache package
    
    * added subset checking
    
    * progress on cache
    
    * progress on cache
    
    * cleanup and progress on caching
    
    * progress on cache
    
    * simplified cache get value
    
    * added cache capacity
    
    * chore: bumped versions

---
+++ packages/cache/src/cache-result.ts
@@ -0,0 +1,57 @@
+export interface UninitializedCacheResult<T> {
+  status: 'uninitialized'
+  value?: T
+  updatedAt: Date
+}
+
+export interface LoadedCacheResult<T> {
+  value: T
+  status: 'loaded'
+  updatedAt: Date
+}
+
+export interface PendingCacheResult<T> {
+  status: 'pending'
+  value?: T
+  updatedAt: Date
+}
+
+export interface FailedCacheResult<T> {
+  value?: T
+  error: unknown
+  status: 'failed'
+  updatedAt: Date
+}
+
+export interface ObsoleteCacheResult<T> {
+  value: T
+  status: 'obsolete'
+  updatedAt: Date
+}
+
+export type CacheResult<T> =
+  | LoadedCacheResult<T>
+  | PendingCacheResult<T>
+  | FailedCacheResult<T>
+  | ObsoleteCacheResult<T>
+  | UninitializedCacheResult<T>
+
+export type CacheWithValue<T> = CacheResult<T> & {
+  value: T
+  updatedAt: Date
+}
+
+export const isLoadedCacheResult = <T>(result: CacheResult<T>): result is LoadedCacheResult<T> =>
+  result.status === 'loaded'
+
+export const isPendingCacheResult = <T>(result: CacheResult<T>): result is PendingCacheResult<T> =>
+  result.status === 'pending'
+
+export const isFailedCacheResult = <T>(result: CacheResult<T>): result is FailedCacheResult<T> =>
+  result.status === 'failed'
+
+export const isObsoleteCacheResult = <T>(result: CacheResult<T>): result is ObsoleteCacheResult<T> =>
+  result.status === 'obsolete'
+
+export const hasCacheValue = <T>(result: CacheResult<T>): result is CacheWithValue<T> =>
+  result.value !== undefined && result.updatedAt instanceof Date
