a211beb860fff79f22ef323d10800035a9430ed6
Lajos <gallayl@users.noreply.github.com>
2023-08-21 21:23:18 +0200

    chore: Monaco async worker config (#320)
    * chore: Monaco async worker config
    
    * chore:bumped versions

--- packages/shades-showcase-app/src/components/worker-config.ts
+++ packages/shades-showcase-app/src/components/worker-config.ts
@@ -1,25 +1,34 @@
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker&inline'
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker&inline'
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker&inline'
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker&inline'
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker&inline'
 import type { Environment } from 'monaco-editor'
+
+const getWorker = async (_: string, label: string) => {
+  let worker
+
+  switch (label) {
+    case 'json':
+      worker = await import('monaco-editor/esm/vs/language/json/json.worker?worker')
+      break
+    case 'css':
+    case 'scss':
+    case 'less':
+      worker = await import('monaco-editor/esm/vs/language/css/css.worker?worker')
+      break
+    case 'html':
+    case 'handlebars':
+    case 'razor':
+      worker = await import('monaco-editor/esm/vs/language/html/html.worker?worker')
+      break
+    case 'typescript':
+    case 'javascript':
+      worker = await import('monaco-editor/esm/vs/language/typescript/ts.worker?worker')
+      break
+    default:
+      worker = await import('monaco-editor/esm/vs/editor/editor.worker?worker')
+  }
+
+  return new worker.default()
+}
 
 self.MonacoEnvironment = {
   ...(self.MonacoEnvironment as Environment),
-  getWorker(_, label) {
-    if (label === 'json') {
-      return new jsonWorker()
-    }
-    if (label === 'css' || label === 'scss' || label === 'less') {
-      return new cssWorker()
-    }
-    if (label === 'html' || label === 'handlebars' || label === 'razor') {
-      return new htmlWorker()
-    }
-    if (label === 'typescript' || label === 'javascript') {
-      return new tsWorker()
-    }
-    return new editorWorker()
-  },
+  getWorker,
 }
