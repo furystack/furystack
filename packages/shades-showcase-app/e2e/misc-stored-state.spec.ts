e5f548c775519928ddcfbbfbce21a7c3fc7b6244
Lajos <gallayl@users.noreply.github.com>
2023-08-22 21:35:58 +0200

    chore: dependency update (#324)
    * chore: dependency update
    
    * regenerated lockfile

---
+++ packages/shades-showcase-app/e2e/misc-stored-state.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test } from '@playwright/test'
+
+test.describe('useStoredState', () => {
+  test('Should rerender once on change', async ({ page }) => {
+    await page.goto('/misc')
+    const testComponent = await page.locator('shades-example-storedstate-change')
+    await testComponent.waitFor({ state: 'visible' })
+
+    await expect(testComponent).toHaveText('Stored state change (0)')
+
+    const input = await testComponent.locator('input')
+    await input.fill('test1')
+    await input.press('Enter')
+
+    await expect(testComponent).toHaveText('Stored state change (1)')
+
+    await input.fill('test2')
+    await input.press('Enter')
+
+    await expect(testComponent).toHaveText('Stored state change (2)')
+  })
+})
