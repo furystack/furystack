746f78c375c2aa5b976e58e78c83904783447d79
Gallay Lajos <gallay.lajos@gmail.com>
2024-03-16 17:32:33 +0100

    collection-service specs

---
+++ packages/shades-common-components/src/services/collection-service.spec.ts
@@ -0,0 +1,36 @@
+import { usingAsync } from '@furystack/utils'
+import { describe, expect, it } from 'vitest'
+import { CollectionService } from './collection-service.js'
+
+const testEntries = [{ foo: 1 }, { foo: 2 }, { foo: 3 }]
+
+describe('CollectionService', () => {
+  describe('Selection', () => {
+    it('Should add and remove selection', async () => {
+      await usingAsync(
+        new CollectionService({
+          defaultSettings: {},
+          loader: async () => ({ count: 3, entries: testEntries }),
+        }),
+        async (collectionService) => {
+          testEntries.forEach((entry) => {
+            expect(collectionService.isSelected(entry)).toBe(false)
+          })
+
+          collectionService.addToSelection(testEntries[0])
+
+          expect(collectionService.isSelected(testEntries[0])).toBe(true)
+          expect(collectionService.isSelected(testEntries[1])).toBe(false)
+          expect(collectionService.isSelected(testEntries[2])).toBe(false)
+
+          collectionService.removeFromSelection(testEntries[0])
+
+          expect(collectionService.isSelected(testEntries[0])).toBe(false)
+
+          collectionService.toggleSelection(testEntries[1])
+          expect(collectionService.isSelected(testEntries[1])).toBe(true)
+        },
+      )
+    })
+  })
+})
