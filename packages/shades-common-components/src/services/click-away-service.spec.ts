8ac7f3c4bd39fac2eb2c7e27ddf1903ecd512f22
Lajos <gallayl@users.noreply.github.com>
2024-03-24 11:55:04 +0100

    Test/minor tests (#366)
    * minor tests
    
    * exclude barrel files
    
    * clickAwayService tests
    
    ---------
    
    Co-authored-by: Gallay Lajos <gallay.lajos@nexogen.hu>

---
+++ packages/shades-common-components/src/services/click-away-service.spec.ts
@@ -0,0 +1,35 @@
+import { describe, expect, it, vi } from 'vitest'
+import { ClickAwayService } from './click-away-service.js'
+
+describe('ClickAwayService', () => {
+  it('Should be constructed and disposed', () => {
+    const service = new ClickAwayService(document.createElement('div'), () => {})
+    service.dispose()
+  })
+
+  it('Should call onClickAway when clicking outside the element', () => {
+    const onClickAway = vi.fn()
+    const div = document.createElement('div')
+    const service = new ClickAwayService(div, onClickAway)
+
+    document.body.appendChild(div)
+    document.body.click()
+
+    expect(onClickAway).toBeCalled()
+
+    service.dispose()
+  })
+
+  it('Should not call onClickAway when clicking inside the element', () => {
+    const onClickAway = vi.fn()
+    const div = document.createElement('div')
+    const service = new ClickAwayService(div, onClickAway)
+
+    document.body.appendChild(div)
+    div.click()
+
+    expect(onClickAway).not.toBeCalled()
+
+    service.dispose()
+  })
+})
