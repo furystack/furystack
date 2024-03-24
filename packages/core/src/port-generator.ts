3f4f98d7dfcced410d0e8ef96658b52234d6aa17
Gallay Lajos <gallay.lajos@gmail.com>
2024-03-24 10:07:59 +0100

    restored port generator with random

--- packages/core/src/port-generator.ts
+++ packages/core/src/port-generator.ts
@@ -1,7 +1,13 @@
-const defaultPort = 16000
+const portGenerator = function* () {
+  const initialPort = 14000 + Math.floor(Math.random() * 2000)
+  let port = initialPort
+  while (true) {
+    yield port++
+  }
+}
 
 /**
  *
- * @returns the next port number starting from 16000
+ * @returns the next port number starting from 14000 to 16000
  */
-export const getPort = () => defaultPort
+export const getPort = () => portGenerator().next().value
