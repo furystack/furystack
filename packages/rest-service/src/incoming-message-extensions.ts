736bb6e965382eec78a77a58fe0844d802e187bf
Gallay Lajos <gallay.lajos@gmail.com>
2024-03-26 14:39:32 +0100

    cleanup incoming message extensions

--- packages/rest-service/src/incoming-message-extensions.ts
+++ packages/rest-service/src/incoming-message-extensions.ts
@@ -1,18 +1,5 @@
-import http from 'http'
-import { readPostBody, readPostBodyRaw } from './read-post-body.js'
-
 declare module 'http' {
   export interface IncomingMessage {
-    readPostBodyRaw: () => Promise<string>
-    readPostBody: <T>() => Promise<T>
     postBody: unknown
   }
-}
-
-http.IncomingMessage.prototype.readPostBody = async function <T>() {
-  return await readPostBody<T>(this)
-}
-
-http.IncomingMessage.prototype.readPostBodyRaw = async function () {
-  return await readPostBodyRaw(this)
 }
