d82dca13211b8deddff22b738fa1917bd9ba8c74
Gallay Lajos <gallay.lajos@gmail.com>
2024-05-23 09:04:13 +0200

    fix port for rest helper tests

--- packages/rest-service/src/helpers.spec.ts
+++ packages/rest-service/src/helpers.spec.ts
@@ -5,6 +5,9 @@ import { HttpAuthenticationSettings } from './http-authentication-settings.js'
 import { useHttpAuthentication, useRestService, useStaticFiles } from './helpers.js'
 import { StaticServerManager } from './static-server-manager.js'
 import { describe, it, expect } from 'vitest'
+import { getPort } from '@furystack/core/port-generator'
+
+const port = getPort()
 
 describe('Injector extensions', () => {
   describe('useHttpAuthentication', () => {
@@ -19,7 +22,7 @@ describe('Injector extensions', () => {
   describe('useRestService()', () => {
     it('Should set up a REST service', async () => {
       await usingAsync(new Injector(), async (i) => {
-        await useRestService({ injector: i, api: {}, root: '/', port: 1234 })
+        await useRestService({ injector: i, api: {}, root: '/', port })
         expect(i.cachedSingletons.get(ApiManager)).toBeDefined()
       })
     })
@@ -28,7 +31,7 @@ describe('Injector extensions', () => {
   describe('useRestService()', () => {
     it('Should set up a REST service', async () => {
       await usingAsync(new Injector(), async (i) => {
-        await useStaticFiles({ injector: i, baseUrl: '/', path: '.', port: 1234 })
+        await useStaticFiles({ injector: i, baseUrl: '/', path: '.', port })
         expect(i.cachedSingletons.get(StaticServerManager)).toBeDefined()
       })
     })
