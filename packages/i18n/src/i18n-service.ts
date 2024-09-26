42e5887962c56ddc16760c0a7b2a6faccf32199f
Gallay Lajos <gallay.lajos@gmail.com>
2024-09-26 09:44:55 +0200

    feat(i18n): Added package stub

---
+++ packages/i18n/src/i18n-service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@furystack/inject'
+
+@Injectable({
+  lifetime: '',
+})
+export class I18NService<Keys extends string> {
+  constructor() {}
+}
---
+++ packages/i18n/src/use-i18n.ts
@@ -0,0 +1,1 @@
+export const useI18N = () => {}
