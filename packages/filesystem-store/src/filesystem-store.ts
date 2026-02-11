ac0c3613249cfcd4d606700f1a145fb091f55eb8
Gallay Lajos <gallay.lajos@gmail.com>
2026-02-11 15:44:04 +0100

    removed lock from filesystem store

--- packages/filesystem-store/src/filesystem-store.ts
+++ packages/filesystem-store/src/filesystem-store.ts
@@ -4,7 +4,8 @@ import type { Constructable } from '@furystack/inject'
 import { EventHub } from '@furystack/utils'
 import type { FSWatcher } from 'fs'
 import { promises, watch } from 'fs'
-import { Lock } from 'semaphore-async-await'
+
+const DEFAULT_TICK_MS = 3000
 
 /**
  * Store implementation that stores info in a simple JSON file
@@ -30,56 +31,40 @@ export class FileSystemStore<T, TPrimaryKey extends keyof T>
   }
 
   public async remove(...keys: Array<T[TPrimaryKey]>): Promise<void> {
-    await this.fileLock.execute(async () => {
-      await this.inMemoryStore.remove(...keys)
-    })
+    await this.inMemoryStore.remove(...keys)
     this.hasChanges = true
   }
 
   public tick: ReturnType<typeof setInterval>
   public hasChanges = false
   public async get(key: T[TPrimaryKey], select?: Array<keyof T>) {
-    return await this.fileLock.execute(async () => {
-      return await this.inMemoryStore.get(key, select)
-    })
+    return await this.inMemoryStore.get(key, select)
   }
 
   public async add(...entries: T[]) {
-    const result = await this.fileLock.execute(async () => {
-      return await this.inMemoryStore.add(...entries)
-    })
+    const result = await this.inMemoryStore.add(...entries)
     this.hasChanges = true
     return result
   }
 
   public async find<TFields extends Array<keyof T>>(filter: FindOptions<T, TFields>) {
-    return await this.fileLock.execute(async () => {
-      return this.inMemoryStore.find(filter)
-    })
+    return this.inMemoryStore.find(filter)
   }
 
   public async count(filter?: FilterType<T>) {
-    return await this.fileLock.execute(async () => {
-      return this.inMemoryStore.count(filter)
-    })
+    return this.inMemoryStore.count(filter)
   }
 
-  private fileLock = new Lock()
   public async saveChanges() {
     if (!this.hasChanges) {
       return
     }
-    try {
-      await this.fileLock.acquire()
-      const values: T[] = []
-      for (const key of this.cache.keys()) {
-        values.push(this.cache.get(key) as T)
-      }
-      await this.writeFile(this.options.fileName, JSON.stringify(values))
-      this.hasChanges = false
-    } finally {
-      this.fileLock.release()
+    const values: T[] = []
+    for (const key of this.cache.keys()) {
+      values.push(this.cache.get(key) as T)
     }
+    await this.writeFile(this.options.fileName, JSON.stringify(values))
+    this.hasChanges = false
   }
 
   public async [Symbol.asyncDispose]() {
@@ -91,7 +76,6 @@ export class FileSystemStore<T, TPrimaryKey extends keyof T>
 
   public async reloadData() {
     try {
-      await this.fileLock.acquire()
       const data = await this.readFile(this.options.fileName)
       const json = data ? (JSON.parse(data.toString()) as T[]) : []
       this.cache.clear()
@@ -103,15 +87,11 @@ export class FileSystemStore<T, TPrimaryKey extends keyof T>
       if (err instanceof Error && (err as { code?: string }).code !== 'ENOENT') {
         throw err
       }
-    } finally {
-      this.fileLock.release()
     }
   }
 
   public async update(id: T[TPrimaryKey], data: T) {
-    await this.fileLock.execute(async () => {
-      return this.inMemoryStore.update(id, data)
-    })
+    await this.inMemoryStore.update(id, data)
     this.hasChanges = true
   }
 
@@ -130,7 +110,7 @@ export class FileSystemStore<T, TPrimaryKey extends keyof T>
     this.primaryKey = options.primaryKey
     this.model = options.model
     this.inMemoryStore = new InMemoryStore({ model: this.model, primaryKey: this.primaryKey })
-    this.tick = setInterval(() => void this.saveChanges(), this.options.tickMs || 3000)
+    this.tick = setInterval(() => void this.saveChanges(), this.options.tickMs || DEFAULT_TICK_MS)
 
     this.inMemoryStore.subscribe('onEntityAdded', ({ entity }) => {
       this.emit('onEntityAdded', { entity })
