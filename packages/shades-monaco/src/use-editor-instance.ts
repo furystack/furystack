a734e43e61cfe79bea1572e4238b6875ec066b00
Gallay Lajos <gallay.lajos@gmail.com>
2026-08-28 15:11:05 +0200

    added code docs

--- packages/shades-monaco/src/use-editor-instance.ts
+++ packages/shades-monaco/src/use-editor-instance.ts
@@ -3,15 +3,28 @@ import { editor } from 'monaco-editor'
 import type { Injector } from '../../inject/src/injector.js'
 import { registerShadesTheme } from './register-shades-theme.js'
 
-export const useEditorInstance = ({
-  element,
-  injector,
-  options,
-}: {
+export type UseEditorInstanceOptions = {
+  /**
+   * The HTML Element that should be used as the base DOM element for monaco `editor.create(...)`
+   */
   element: HTMLElement
+  /**
+   * The injector instance to retrieve neccessary services, e.g.: ThemeProviderService
+   */
   injector: Injector
+  /**
+   * Additional options that can be passed to the Monaco Editor's `editor.create(...)` method
+   */
   options: editor.IStandaloneEditorConstructionOptions
-}) => {
+}
+
+/**
+ * Utility method to manage Monaco Editor Instance
+ *
+ * @param param0 The Options to use for the editor instance
+ * @returns The created Editor Instance
+ */
+export const useEditorInstance = ({ element, injector, options }: UseEditorInstanceOptions) => {
   const themeName = registerShadesTheme({ injector })
 
   const editorInstance = editor.create(element, {
