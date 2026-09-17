e11f71ed784d1164b08e00dfabffaa67413e172f
Gallay Lajos <gallay.lajos@gmail.com>
2026-09-17 17:57:37 +0200

    added schema test

--- packages/shades-monaco/src/monaco-editor.spec.tsx
+++ packages/shades-monaco/src/monaco-editor.spec.tsx
@@ -3,9 +3,10 @@ import { createComponent, flushUpdates, initializeShadeRoot } from '@furystack/s
 import { defaultDarkTheme, ThemeProviderService } from '@furystack/shades-common-components'
 import { usingAsync } from '@furystack/utils'
 import 'monaco-editor/features/register.all'
+import type { JSONSchema } from 'monaco-editor/languages/features/json/register.js'
 import 'monaco-editor/languages/register.all'
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
-import { MonacoEditor } from './monaco-editor.js'
+import { MonacoEditor, type MonacoEditorProps } from './monaco-editor.js'
 
 vi.hoisted(() => {
   global.document.queryCommandSupported = () => false
@@ -56,6 +57,72 @@ describe('MonacoEditor', { timeout: 30 * 1000 }, () => {
           { timeout: 20 * 1000 },
         )
         .toContain(exampleValue)
+    })
+  })
+
+  it('Should use a JSON Schema', async () => {
+    await usingAsync(createInjector(), async (injector) => {
+      const rootElement = document.getElementById('root') as HTMLDivElement
+
+      injector.get(ThemeProviderService).setAssignedTheme(defaultDarkTheme)
+
+      const exampleValue = JSON.stringify(
+        {
+          username: 'Fury Frank',
+          age: 12,
+          isAdmin: true,
+        },
+        undefined,
+        2,
+      )
+
+      const userSchema = {
+        type: 'object',
+        properties: {
+          username: { type: 'string', description: 'The unique handle for the user account.' },
+          age: { type: 'integer', minimum: 18, description: 'User must be at least 18 years old.' },
+          isAdmin: { type: 'boolean', default: false },
+        },
+        required: ['username', 'age'],
+        additionalProperties: false,
+      } satisfies JSONSchema
+
+      const onMarkersChangeFn = vi.fn()
+      const onValueChangeFn = vi.fn()
+
+      initializeShadeRoot({
+        injector,
+        rootElement,
+        jsxElement: (
+          <MonacoEditor
+            options={{
+              language: 'json',
+            }}
+            value={''}
+            onMarkersChange={onMarkersChangeFn}
+            onValueChange={onValueChangeFn}
+            schema={{
+              uri: 'furystack://my-custom-schema.json',
+              diagnosticOptions: {
+                schemaValidation: 'error',
+              },
+              jsonSchema: userSchema,
+            }}
+          />
+        ),
+      })
+
+      await flushUpdates()
+
+      const editor = document.querySelector('monaco-editor') as HTMLTextAreaElement & JSX.Element<MonacoEditorProps>
+
+      editor.props.value = exampleValue
+      editor.updateComponent()
+
+      await flushUpdates()
+
+      await expect.poll(() => onValueChangeFn, { timeout: 10 * 1000 }).toHaveBeenCalledOnce()
+      expect(onValueChangeFn).toHaveBeenCalledExactlyOnceWith(exampleValue)
     })
   })
 })
